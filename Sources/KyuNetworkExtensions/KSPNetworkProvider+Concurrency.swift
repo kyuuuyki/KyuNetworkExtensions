8cc38ff4338f749b7535a952c7ba522f6e1f063c
Chayanon Ardkham <develop@chayanon.dev>
2023-09-06 16:40:47 +0700

    Concurrency - Make KSPNetworkProvider's concurrency throwable

--- Sources/KyuNetworkExtensions/KSPNetworkProvider+Concurrency.swift
+++ Sources/KyuNetworkExtensions/KSPNetworkProvider+Concurrency.swift
@@ -16,10 +16,15 @@ public extension KSPNetworkProvider {
 		
 		// MARK: PROVIDED PUBLIC FUNCTIONS
 		/// Perform request without object mapping.
-		public func requestPlain(route: T) async -> (Result<Response, E>) {
-			return await withCheckedContinuation { continuation in
+		public func requestPlain(route: T) async throws -> Response {
+			return try await withCheckedThrowingContinuation { continuation in
 				provider.requestPlain(route: route) { result in
-					continuation.resume(returning: result)
+					switch result {
+					case .success(let response):
+						continuation.resume(returning: response)
+					case .failure(let error):
+						continuation.resume(throwing: error)
+					}
 				}
 			}
 		}
@@ -31,8 +36,8 @@ public extension KSPNetworkProvider {
 			route: T,
 			maxAttempts: Int = 0,
 			responseHandler: @escaping (Response) -> Void = { _ in }
-		) async -> (Result<O?, E>) {
-			return await withCheckedContinuation { continuation in
+		) async throws -> O? {
+			return try await withCheckedThrowingContinuation { continuation in
 				provider.requestObject(
 					type: type,
 					nestedAt: path,
@@ -40,7 +45,12 @@ public extension KSPNetworkProvider {
 					maxAttempts: maxAttempts,
 					responseHandler: responseHandler
 				) { result in
-					continuation.resume(returning: result)
+					switch result {
+					case .success(let decodable):
+						continuation.resume(returning: decodable)
+					case .failure(let error):
+						continuation.resume(throwing: error)
+					}
 				}
 			}
 		}
@@ -52,8 +62,8 @@ public extension KSPNetworkProvider {
 			route: T,
 			maxAttempts: Int = 0,
 			responseHandler: @escaping (Response) -> Void = { _ in }
-		) async -> (Result<[O]?, E>) {
-			return await withCheckedContinuation { continuation in
+		) async throws -> [O]? {
+			return try await withCheckedThrowingContinuation { continuation in
 				provider.requestObjects(
 					type: type,
 					nestedAt: path,
@@ -61,7 +71,12 @@ public extension KSPNetworkProvider {
 					maxAttempts: maxAttempts,
 					responseHandler: responseHandler
 				) { result in
-					continuation.resume(returning: result)
+					switch result {
+					case .success(let decodables):
+						continuation.resume(returning: decodables)
+					case .failure(let error):
+						continuation.resume(throwing: error)
+					}
 				}
 			}
 		}
